45373a5ce835fe3a36af3fd912495f5b6b97f4d0
MTraveller <26313521+MTraveller@users.noreply.github.com>
2022-03-12 20:39:06 +0000

    Add user URL request loop to repeat until URL is valid

--- run.py
+++ run.py
@@ -5,12 +5,18 @@ def get_url_link():
     """
     Get website link input from the user
     """
-    print("Please enter your URL")
-    print("The URL MUST include HTTP, subdomain, domain and tld\n")
-    print("Example: https://en.wikipedia.org/wiki/Python_(programming_language)\n")
+    while True:
+        print("Please enter your URL")
+        print("The URL MUST include HTTP, subdomain, domain and tld\n")
+        print("Example: https://en.wikipedia.org/wiki/Python_(programming_language)\n")
 
-    url_link = input("Enter your url here: ")
-    validate_url(url_link)
+        url_link = input("Enter your url here: ")
+
+        if validate_url(url_link):
+            print("URL is valid!")
+            break
+
+    return url_link
 
 
 def validate_url(url):
@@ -33,6 +39,9 @@ def validate_url(url):
             )
     except ValueError as validate_error:
         print(f"\nInvalid link: {validate_error}, please try again.\n")
+        return False
+
+    return True
 
 
-get_url_link()
+url = get_url_link()
