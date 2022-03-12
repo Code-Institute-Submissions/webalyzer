7df6c38a9ad2157af1357cfa622b99754fc89ef4
MTraveller <26313521+MTraveller@users.noreply.github.com>
2022-03-12 02:14:04 +0000

    removed os import

--- run.py
+++ run.py
@@ -1,12 +1,4 @@
 import requests
-import os
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Static files (CSS, JavaScript, Images)
-STATIC_URL = '/assets/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'assets'),)
 
 # Making a GET request
 r = requests.get('https://api.github.com/users/mtraveller')
