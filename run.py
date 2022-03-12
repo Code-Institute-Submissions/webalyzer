bf632e2a4cdc357b488e0a4e3aec44e89ed95b98
MTraveller <26313521+MTraveller@users.noreply.github.com>
2022-03-12 01:09:46 +0000

    testing import os for assets folder

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import requests
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/assets/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'assets'),)
+
+# Making a GET request
+r = requests.get('https://api.github.com/users/mtraveller')
+  
+# check status code for response received
+# success code - 200
+print(r)
+  
+# print content of request
+print(r.content)
