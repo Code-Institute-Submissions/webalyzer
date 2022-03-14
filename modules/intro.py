77dbc09a8d7c157379c169da5f8bbd0bdc42fc6c
MTraveller <26313521+MTraveller@users.noreply.github.com>
2022-03-14 17:39:19 +0000

    cleanup print  for testing import modules

--- modules/intro.py
+++ modules/intro.py
@@ -1,10 +1,7 @@
 """
 init run intro
 """
-from modules.validate_requested_url import print_message, get_url_link
-
-message = print_message
-print(message)
+from modules.validate_requested_url import get_url_link
 
 
 def print_intro():
