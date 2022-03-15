5d0ccecc5fc6883cf5ace50f5837baa195bfb595
MTraveller <26313521+MTraveller@users.noreply.github.com>
2022-03-15 09:30:13 +0000

    run clear terminal on startup

--- run.py
+++ run.py
@@ -1,11 +1,13 @@
 """
 Init the app
 """
+from subprocess import run
 from time import sleep
 from art import tprint
 from modules.intro import print_intro
 
 
+run('clear', check=True)
 tprint("Welcome to", font="Small Slant", sep="\n")
 sleep(.2)
 tprint("Websichanic", font="Small Slant", sep="\n")
