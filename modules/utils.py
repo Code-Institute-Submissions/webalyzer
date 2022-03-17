58de67641fb9ecc17ba4bc3ee131503c2504316d
MTraveller <26313521+MTraveller@users.noreply.github.com>
2022-03-17 21:01:04 +0000

    Remove commented out old code of validate_input_answer function

--- modules/utils.py
+++ modules/utils.py
@@ -4,9 +4,17 @@
 import sys
 from time import sleep
 from art import tprint
-from modules.slow_print import sprint  # , wprint
+from modules.slow_print import sprint
+from modules.prints import WEBALYZER, WRONG
 
-WRONG = "\x1b[1;31mWrong answer please try again...\x1b[0;0m"
+
+def print_yes_no():
+    """
+        Print "use yes / no" above input
+    """
+    sprint("Please use '\x1b[1;32my\x1b[0;0m' "
+           "for \x1b[1;32mYes\x1b[0;0m and "
+           "'\x1b[1;31mn\x1b[0;0m' for \x1b[1;31mNo\x1b[0;0m")
 
 
 def print_intro_welcome():
@@ -27,13 +35,14 @@ def print_brand_name():
     sleep(.5)
 
 
-def print_yes_no():
+def print_about():
     """
-        Print use yes / no for input
+        Prints Webalyzer's short story
     """
-    sprint("Please use '\x1b[1;32my\x1b[0;0m' "
-           "for \x1b[1;32mYes\x1b[0;0m and "
-           "'\x1b[1;31mn\x1b[0;0m' for \x1b[1;31mNo\x1b[0;0m")
+
+    for line in WEBALYZER:
+        sleep(1)
+        print(line)
 
 
 def del_last_line():
@@ -47,7 +56,7 @@ def del_last_line():
 
 def del_last_lines_up(times):
     """
-        Function to delete last line up(s)
+        Function to delete last lines up to (times)
     """
     for _ in range(times):
         # ACSII codes based on
@@ -62,48 +71,9 @@ def del_last_lines_up(times):
     sleep(1)
 
 
-# def validate_input_answer(options, answer):
-#     """
-#         Validates if input answer given aligns with
-#         the expected input options
-#     """
-#     # if len(answer) > 1:
-#     #     sleep(1)
-#     #     print(WRONG)
-#     #     del_last_lines_up(4)
-
-#     #     return True
-
-#     result = answer in options
-#     if not result:
-#         sleep(1)
-#         sprint(WRONG)
-#         print(result)
-#         # del_last_lines_up(4)
-
-#         return True, result
-
-#     return False, result
-
-
-# class Answer:
-#     """
-#         Converts the string to lowercase,
-#         strips any before and after whitespace
-#         and replaces any spaces between the letters
-#         if any.
-#     """
-
-#     def __init__(self, answer):
-#         self.answer = answer
-
-#     def __str__(self):
-#         return str(self.answer).lower().strip().replace(" ", "")
-
-
 class Validate:
     """
-        Validates unser input
+        Validates user input
     """
 
     def __init__(self, options, answer):
