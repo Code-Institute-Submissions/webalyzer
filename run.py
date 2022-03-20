cdca0b0ebea14e9499dcc9f7c3c0a4bd983b590e
MTraveller <26313521+MTraveller@users.noreply.github.com>
2022-03-20 23:37:23 +0000

    Refactored unnecessary function start_intro() and refactored imports

--- run.py
+++ run.py
@@ -2,19 +2,10 @@
 Init the app
 """
 from subprocess import run
-from modules.utils import print_intro_welcome
+from modules.prints import print_intro_welcome
 from modules.intro import print_intro
 
 
 run('clear', check=True)
 print_intro_welcome()
-
-
-def start_intro():
-    """
-    Initialize intro
-    """
-    return print_intro()
-
-
-start_intro()
+print_intro()
