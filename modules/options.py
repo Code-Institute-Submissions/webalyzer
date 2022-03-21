030b9c8258a0aabc169ed876fcd11fc71b1eb5a8
MTraveller <26313521+MTraveller@users.noreply.github.com>
2022-03-21 19:34:15 +0000

    Add code to validation options and refactor print options screen

--- modules/options.py
+++ modules/options.py
@@ -1,14 +1,14 @@
 """
 init run options
 """
-# from subprocess import run
+
 # from time import sleep
 # from getch import pause
 # from modules.slow_print import sprint  # , wprint
 # from modules.utils import delete_last_line
 from modules.utils import Validate
 from modules.validate.validate_code import Html
-from modules.prints import CHOICES, QUITORNEW
+from modules.prints import run_choices_screen
 
 
 def run_choices(validated_url):
@@ -16,11 +16,7 @@ def run_choices(validated_url):
     Function to run choices
     """
 
-    print(QUITORNEW)
-    print()
-    for line in CHOICES:
-        print(line)
-    print()
+    run_choices_screen()
 
     while True:
         # Creating a new Validate object
@@ -31,10 +27,16 @@ def run_choices(validated_url):
             chosen_num[0], chosen_num[1])
         if (
             response_num[0] and
-            (response_num[1] == '1' or '2' or '3' or 'new')
+            (response_num[1] == '1' or '2' or '3')
         ):
             if response_num[1] == '1':
                 validation = Html(validated_url)
-                errors = validation.err()
-                print(errors)
-                break
+                validation.err()
+
+            elif response_num[1] == '2':
+                validation = Css(validated_url)
+                validation.err()
+
+            else:
+                validation = Javascript(validated_url)
+                validation.err()
