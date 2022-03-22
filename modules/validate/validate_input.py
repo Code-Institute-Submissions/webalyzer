015651fdeabeb3304f95ddb7dda692b51b7f9bff
MTraveller <26313521+MTraveller@users.noreply.github.com>
2022-03-22 18:05:07 +0000

    Refactor utils module and rename to validate_input and move to validate folder

--- modules/validate/validate_input.py
+++ modules/validate/validate_input.py
@@ -1,36 +1,11 @@
 """
-    init utils module
+    init validate input module
 """
+
 import sys
 from time import sleep
 from modules.slow_print import sprint
-from modules.prints import WRONG, print_outro
-
-
-def del_last_line():
-    """
-        Function to delete last line
-    """
-
-    # delete last line
-    sys.stdout.write('\r\x1b[2K')
-
-
-def del_last_lines_up(times):
-    """
-        Function to delete lines up to (x times)
-    """
-
-    for _ in range(times):
-        # ACSII codes based on
-        # https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797
-
-        # Move one line up \r to the start of the line
-        sys.stdout.write('\r\x1b[1A')
-        # Delete the last line
-        sys.stdout.write('\x1b[2K')
-    print()
-    sleep(.2)
+from modules.prints import WRONG, print_outro, del_last_lines_up
 
 
 class Validate:
