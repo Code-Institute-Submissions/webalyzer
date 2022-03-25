0504355d0c4df0db8aea9ed024cf7905aba4dac5
MTraveller <26313521+MTraveller@users.noreply.github.com>
2022-03-25 22:21:28 +0000

    Refactor except & optimize targeting of errors & fix input another loop bug

--- modules/validate/validate_code.py
+++ modules/validate/validate_code.py
@@ -18,6 +18,25 @@ from modules.prints import (print_brand_name,
 from modules.regex import VALID_SUBTRING
 
 
+def run_options():
+    """
+        Function to display
+        options.
+
+        ....
+        Args:
+            None
+        ....
+        Returns:
+            No explicit return set.
+        ....
+    """
+
+    del_last_lines_up(1000)
+    print_brand_name()
+    run_choices_screen()
+
+
 def error_choose_another_option():
     """
         Function to display another
@@ -38,9 +57,7 @@ def error_choose_another_option():
     # Validating the input against valid items
     another[0].validate_input(another[0], another[1])
 
-    del_last_lines_up(1000)
-    print_brand_name()
-    run_choices_screen()
+    run_options()
 
 
 def choose_another_option():
@@ -57,13 +74,15 @@ def choose_another_option():
             No explicit return set.
         ....
     """
-    del_last_lines_up(2)
+    del_last_lines_up(1)
     print("Well done, there were no errors!")
+
     # Creating a new Validate object
     another = Validate.another_cls()
-
     # Validating the input against valid items
     another[0].validate_input(another[0], another[1])
+
+    run_options()
 
 
 class ValidateCode:
@@ -123,31 +142,10 @@ class ValidateCode:
                         sleep(1.2)
                         del_last_lines_up(1)
 
-                    if self.res_check != 200:
-                        raise HTTPError(
-                            f"Response: {self.res_check}"
-                        )
-
-                if self.res_check in (200, 201, 202):
-                    if self.option == '1':
-                        print("Checking Successfull!")
-                        sleep(1)
-                        del_last_lines_up(1)
-                        print("Retriving Results :)")
-                        res = re_s.get(self.html)
-                        sleep(1)
-                        del_last_lines_up(1)
-                    else:
-                        print("Checking Successfull!")
-                        sleep(1)
-                        del_last_lines_up(1)
-                        print("Retriving Results :)")
-                        res = re_s.get(self.css)
-                        sleep(1)
-                        del_last_lines_up(1)
-
-            self.data = BeautifulSoup(res.content, 'html5lib')
-            self.data.prettify()
+                if self.res_check not in [200, 201, 202]:
+                    raise HTTPError(
+                        f"Response: {self.res_check}"
+                    )
 
         except HTTPError as http_err:
             print("Validation Service Error: "
@@ -156,6 +154,28 @@ class ValidateCode:
         except requests.exceptions.ConnectionError:
             print("This validation service can't be "
                   "reached, please try again, later.")
+
+        else:
+            if self.res_check:
+                if self.option == '1':
+                    print("Checking Successfull!")
+                    sleep(1)
+                    del_last_lines_up(1)
+                    print("Retriving Results :)")
+                    res = re_s.get(self.html)
+                    sleep(1)
+                    del_last_lines_up(1)
+                else:
+                    print("Checking Successfull!")
+                    sleep(1)
+                    del_last_lines_up(1)
+                    print("Retriving Results :)")
+                    res = re_s.get(self.css)
+                    sleep(1)
+                    del_last_lines_up(1)
+
+            self.data = BeautifulSoup(res.content, 'html5lib')
+            self.data.prettify()
 
     def __str__(self):
         # __str__ avoids sending the
@@ -184,10 +204,10 @@ class Html(ValidateCode):
         super().__init__(option, data)
 
         try:
-            self.all_li = self.data.find_all('li')
-            if self.all_li == "'Html' object has no attribute 'data'":
+            self.all_lis = self.data.find('ol')
+            if self.all_lis == "'Html' object has no attribute 'data'":
                 raise AttributeError(
-                    f"No Response: {self.all_li}"
+                    f"No Response: {self.all_lis}"
                 )
         except AttributeError as data_err:
             del_last_lines_up(3)
@@ -201,7 +221,7 @@ class Html(ValidateCode):
 
     def err(self):
         """
-            Function to filter and beautify
+            Function to filter
             errors for the terminal.
 
             ....
@@ -209,52 +229,53 @@ class Html(ValidateCode):
                 str(data=(scraped HTML of validated service))
             ....
             Returns:
-                Class is not set to explicitly
-                return anything other than printing
-                in the terminal of either errors
-                or a no error message.
+                Class medthod is not set to
+                return True after printed in the terminal
+                of either errors or a no error message.
             ....
         """
 
         try:
-            all_lis = self.data.find_all('li', class_='error')
-            if all_lis == "'Html' object has no attribute 'data'":
+            all_errors = self.all_lis.find_all('li')
+            if all_errors == "'Html' object has no attribute 'data'":
                 raise AttributeError()
-
-            print("Processing Results..")
-
-            errors = []
-            location = []
-            for error in all_lis:
-                err = error.find('p').text
-                loc = error.find('p', {'class': 'location'}).text
-                errors.append(err)
-                location.append(loc)
-
-            error_list = list(zip(errors, location))
-            error_list = list(set(error_list))
-
-            del_last_lines_up(5)
-
-            if len(error_list) != 0:
-                print("We received the following errors:\n")
-
-                for error in error_list:
-                    for line in error:
-                        print(line)
-                    sleep(.4)
-                    print()
-
-                pause(message="\x1b[3mPress any key to continue...\x1b[23m")
-                del_last_lines_up(1000)
-                print_brand_name()
-                run_choices_screen()
-
-            else:
-                choose_another_option()
 
         except AttributeError:
             error_choose_another_option()
+            return True
+
+        else:
+            print("Processing Results..")
+            sleep(1)
+            errors_list = []
+            for list_item in all_errors:
+                count = 0
+                try:
+                    for li in list_item.find('li', {'class': 'error'}):
+                        if count == 2:
+                            continue
+                        errors_list.append(li.text)
+                        count += 1
+                except TypeError:
+                    choose_another_option()
+                    return True
+
+            del_last_lines_up(5)
+
+            print("We received the following errors:\n")
+
+            for error in errors_list:
+                for line in error:
+                    print(line)
+                sleep(.4)
+                print()
+
+            pause(message="\x1b[3mPress any key to continue...\x1b[23m")
+            del_last_lines_up(1000)
+            print_brand_name()
+            run_choices_screen()
+
+            return True
 
 
 class Css(ValidateCode):
@@ -267,9 +288,9 @@ class Css(ValidateCode):
             str(self.data=(scraped HTML of validated service))
         ....
         Returns:
-            Class is not set to explicitly other
-            than passed on to err() after a try for error
-            has been made.
+            Class medthod is not set to
+            return True after printed in the terminal
+            of either errors or a no error message.
         ....
     """
 
@@ -277,11 +298,12 @@ class Css(ValidateCode):
         super().__init__(option, data)
 
         try:
-            self.trs = self.data.find_all('tr', {'class': 'error'})
+            self.trs = set(self.data.find_all('tr', {'class': 'error'}))
             if self.trs == "'Css' object has no attribute 'data'":
                 raise AttributeError(
                     f"No Response: {self.trs}"
                 )
+
         except AttributeError as data_err:
             del_last_lines_up(3)
             print("Ohh.. noo.. There's an error!\n"
@@ -292,9 +314,12 @@ class Css(ValidateCode):
                   "that the validator has an issue.\n")
             sleep(3)
 
+        print("Processing Results..")
+        sleep(1)
+
     def err(self):
         """
-            Function to filter and beautify
+            Function to filter
             errors for the terminal.
 
             ....
@@ -314,23 +339,26 @@ class Css(ValidateCode):
             if self.trs in self.trs == "'Css' object has no attribute 'trs'":
                 raise AttributeError()
 
-            print("Processing Results..")
+        except AttributeError:
+            error_choose_another_option()
+            return True
+
+        else:
             for tr in self.trs:
                 tr_text = tr.text
 
                 valid_text = re.findall(VALID_SUBTRING, tr_text)
                 for idx in valid_text:
                     index = valid_text.index(idx)
-                    if not len(idx) > 8:
+                    if not len(idx) > 6:
                         continue
                     replace_text = re.sub(' : ', ': ', idx)
                     valid_text[index] = replace_text
                 error_list.append(valid_text)
 
-            del_last_lines_up(5)
-
             if len(error_list) != 0:
                 error_list = set(tuple(err_sub) for err_sub in error_list)
+                del_last_lines_up(5)
                 print("We received the following errors:\n")
 
                 for err_l in error_list:
@@ -349,5 +377,4 @@ class Css(ValidateCode):
             else:
                 choose_another_option()
 
-        except AttributeError:
-            error_choose_another_option()
+        return True
